ecfa921a533e5627109c0ca6a93248457a5704c8
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-27 15:59:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  function reverse(word) {
+    const wordArray = word.split("");
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join("");
+    return reversedWord;
+  }
+  
+  function isPalindrome(word) {
+    // Write your algorithm here
+    const reversedWord = reverse(word)
+    if(word === reversedWord){
+      return true;
+    }else{
+      return false;
+    }
+    return reversedWord
+  }
+  
+  /* 
+    Add your pseudocode here
+    function that return reversed word
+    conditional statement that check if the word is the same as the reversed
+  */
+  
+  /*
+    Add written explanation of your solution here
+    i need to write a function that returns a string in a reversed order
+    i need to check if the reversed string is same the passed strng, i return true.
+  */
+  
+  // You can run `node index.js` to view these console logs
+  
 }
 
 /* 
